4065c64685509812328d7d599d061340582a58df
alaanousir <s-alaanousir@zewailcity.edu.eg>
2019-12-03 23:55:22 +0200

    Motion estimation and compensation progress

--- Encoder.py
+++ Encoder.py
@@ -3,13 +3,60 @@ import cv2
 import sys
 sys.path.insert(0,'JPEG-Compression')
 import encoder as e
+import main as m
 
 ## The following functions will be called in the main.py of the video compression
 
-# Reshape the image using the encoder functions in JPEG
+def reshape_image(image, box_size = 16):
 
-# Second, get sub-images. Now, we have a frame that is divided into 16x16 macroblocks.
+    n_rows = np.int(np.floor(image.shape[0]/box_size))
+    n_cols = np.int(np.floor(image.shape[1]/box_size))
 
+    image_array = cv2.resize(vid_frame[100], dsize=(n_rows*box_size, n_cols*box_size))
+    return image_array
+
+def get_sub_images(image_array, box_size=16):
+    """
+    Gets a grayscale image and returns an array of (box_size, box_size) elements
+    Args:
+        image_array (numpy ndarray): Image input we want to divide to box
+                                     sub_images.
+         Should have shape (length, width, n_channels) where length = width
+          e. g. n_channels = 3 for RGB
+         box_size (int): Size of the box sub images
+    Returns:
+        divided_image (numpy ndarray, dtype = "uint8"): array of divided images
+         - should have a shape of (X, box_size, box_size, n_channels).
+        n_rows: number of rows or blocks
+        n_cols: number of columns in image
+          the number of blocks is n_rows*n_cols
+    """
+    n_rows = np.int(image_array.shape[0]/box_size)
+    n_cols = np.int(image_array.shape[1]/box_size)
+
+    # make the image into a square to simplify operations based
+    #  on the smaller dimension
+    # d = min(n_cols, n_rows)
+
+    # Note: images are converted to uint8 datatypes since they range between
+    #  0-255. different datatypes might misbehave (based on my trials)
+    image_blocks = np.asarray([np.zeros((box_size, box_size), dtype='uint8')
+                               for i in range(n_rows*n_cols)], dtype='uint8')
+
+    # break down the image into blocks
+    c = 0
+    for i in range(n_rows):
+        for j in range(n_cols):
+            image_blocks[c] = image_array[i*box_size: i*box_size+box_size,
+                                          j*box_size:j*box_size+box_size]
+            c += 1
+
+    # If you want to reconvert the output of this function into images,
+    #  use the following line:
+    # block_image = Image.fromarray(output[idx])
+
+    return image_blocks, n_rows, n_cols
+    
 
 def rmse(im, ref_im):
     """
@@ -20,32 +67,12 @@ def rmse(im, ref_im):
     Returns:
         rmse: root mean squared error as a metric to compare between the original image and the reconstructed
     """
-    error = im - reconstructed_image
+    error = ref_im - im
     mse = np.sum(np.square(error)) / (im.shape[0] * im.shape[1])
     rmse = np.sqrt(mse)
 
     return rmse
 
-def motion_vectors(coordinates, matching_coordinates , block_size):
-    """
-    Gets the coordinates of the current image block, and the coordinates of the matching search area
-    and returns the motion vector
-    Args:
-       coordinates: a tuple containing the x and y coordinates of the image block
-       matching_coordinates: a tuple containing the i and j coordinates of the matching image block
-    Returns:
-        motion_vector: the x and y distances between the x-y coordinates of the image block and the i-j coordinates of the matching block
-    """
-    if x>i:
-        x_dir = x-i+0.5*block_size
-    else:
-        x_dir = i-x+0.5*block_size
-    if y>j:
-        y_dir = y-j+0.5*block_size
-    else:
-        y_dir = j-y+0.5*block_size
-        
-    return (y_dir,x_dir)
 
 def motion_estimation(ref_frame, current_block, coordinates, search_size =64):
     """
@@ -58,56 +85,81 @@ def motion_estimation(ref_frame, current_block, coordinates, search_size =64):
         coordinates: a tuple containing the x and y coordinates of the image block
         search_size: is half the size of the search_area as a whole.
     Returns:
-        residual: np array of the residual macroblock
         motion_vector: the coordinate distance change (in pixel units) between the current frame and reference frame.
     """
-    ###############
-    #SPECIAL CASES NOT HANDLED YET
-    ############
+    # Pad the image with the search_size specified at the borders of the image
+    # This ensures that the search area will never be outside the boundaries of the image
+    h, w = ref_frame.shape
+    padded = np.zeros(( h + search_size*2, w + search_size*2))  # Initialize an array of zeros 
+    padded[search_size : h+search_size, search_size : w+search_size] = ref_frame  # Add the reference frame to this array
                
     # find the matching 16x16 block from the search_area
     y, x = coordinates
-    search_area = ref_frame[(y - search_size) : (y + search_size), (x - search_size) : (x + search_size)]
+    search_area = padded[(y) : (y + search_size), (x) : (x + search_size)]
     block_size = current_block.shape[0]
     loss_prev = 1000
-    for j in range(search_size - block_size):
-        for i in range(search_size - block_size):
+    for j in range(search_size):
+        for i in range(search_size):
             loss = rmse(current_block, search_area[i:i+block_size,i:i+block_size])
             if loss < loss_prev:
                 loss_prev = loss
                 matching_coordinates = (j,i)
-    motion_vectors = motion_vectors(coordinates , matching_coordinates , block_size)     
+    motion_vectors = (j-y, i-x)   
     
-    return residual, vector_vectors
+    return motion_vectors
    
 
-def motion_estimation_to_all(prev_frame, current_frame):
+def motion_estimation_to_all(ref_frame, current_frame, n_rows, n_cols, search_size =64):
     """
     Gets the reference frame (previous frame) and current frame. The prev_frame could be the k-nth frame where k is the current frame.
     and performs motion estimation on all 16x16 macroblocks in the current frame.
     Args:
-        prev_frame: np array of the reference frame image divided into 16x16 macroblocks
-        current_frame: current frame image divided into 16x16 macroblocks.
+        ref_frame: np array of the reference frame image
+        current_frame: current frame image divided into 16x16 macroblocks of shape 
         - should have a shape of (X, macroblock_size, macroblock_size)
+        n_rows: number of rows
+        n_cols: number of columns
+        search_size: the search area is search_size*2
     returns:
-        residuals: np array of the residual macroblocks.
         motion_vectors: np array of size(X, 2) where each macroblock has a motion vector represented in 2 values in the x and y coordinates 
     """
-    #motion_vectors, residuals = np.array([motion_estimation(search_area, block)
-    #                              for ......])
-    return residuals, motion_vectors
+    h, w = ref_frame.shape
+    macroblock_size = current_frame.shape[1]
+    motion_vectors = []
+    # Tuple containing the coordinates of any given image macroblock.
+    # The coordinates of the centre pixel of the image macroblock
+    for j in range(n_rows):
+        for i in range(n_cols):
+            motion_vectors.append(motion_estimation(ref_frame, current_frame[i],  (j,i), search_size))
+                                  
+    return motion_vectors
 
 
-def motion_prediction(prev_frame, motion_vectors):
+
+
+
+def motion_compensation(ref_frame, motion_vectors):
     """ 
     Gets the previous frame and the motion_vectors and returns the predicted current frame.
     Args:
-        prev_frame: np array of the reference frame image divided into 16x16 macroblocks 
+        ref_frame: np array of the reference frame image divided into 16x16 macroblocks 
         motion_vectors: np array of size(X, 2) that we got from motion_estimation block.
     Returns:
         predicted_frame: np array of the predicted frame
     """ 
     return predicted_frame
+
+def residual(current_frame, predicted_frame):
+    """
+    Gets the current frame and predicted_frame and subtracts them to return the residual frame.
+    Args: current_frame: current frame image divided into 16x16 macroblocks.
+        - should have a shape of (X, macroblock_size, macroblock_size)
+          predicted_frame: np array of the predicted frame
+    Returns: 
+          residual_frame: np array of the residual macroblock
+    """      
+    return current_frame - predicted_frame 
+            
     
     
 def spatial_model(residual_frame):
@@ -116,16 +168,30 @@ def spatial_model(residual_frame):
     Args:
         residual_frame: np array of the residual frame of shape (X, macroblock_size, macroblock_size) that will be encoded
     returns:
-        coefficients (numpy ndarray): 1d array representing the residual frame
+        quantized_coeff (numpy ndarray): 1d array representing the residual frame
     """
-    return e.apply_dct_to_all(residual_frame)                     
+    coeff = e.apply_dct_to_all(residual_frame)
+    quantized_coeff = e.quantize(coeff, m.table_8_high)
+    return quantized_coeff
+
+def reconstructed(predicted_frame, quantized_coeff):
+    """
+    Gets the predicted_frame and quantized_coefficients and transforms back the coefficients to residual frame 
+    and gets the reconstructed image by adding the predicted_frame to the reconstructed residual frame.
+    Args:
+        predicted_frame: np array of the predicted frame
+        quantized_coeff (numpy ndarray): 1d array representing the residual frame
+    Returns:
+        reconstructed_current
+    """
+    return reconstructed_current
     
     
-def arithmetic_encode(coefficients, motion_vectors):
+def arithmetic_encode(quantized_coeff, motion_vectors):
     """
     Applied arithmetic coding to the 1d array representing the bit stream
     Args:
-        coefficients (numpy ndarray): 1d array representing the residual frame
+        quantized_coeff (numpy ndarray): 1d array representing the residual frame
         motion_vectors (numpy ndarray): 2d array represnting the motion vector (component in x and y direction) 
         
     Returns:
